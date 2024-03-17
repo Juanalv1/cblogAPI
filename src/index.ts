d0af03790138ad40e81d93bcbe69f4d54f27a569
Juan <elquesififa@gmail.com>
2024-03-16 23:33:46 -0500

    updated cors config

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://cblog-39q2urz3i-juanalv1.vercel.app',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
