44c37e7a75f7bb9da31dea6d44fb6926c6cd4292
Juan <elquesififa@gmail.com>
2024-03-17 12:26:15 -0500

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-  origin: 'https://cblog-39q2urz3i-juanalv1.vercel.app',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
